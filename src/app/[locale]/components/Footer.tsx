4ffe5ca368e89d9c0cf364bd12a5e228970581bc
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2024-07-13 11:17:59 -0700

    added footer links

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,25 +1,40 @@
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 export const Footer = () => {
+  const t = useTranslations('')
   return (
-    <div className="flex flex-row justify-center my-10 footer">
-    <div className='flex flex-col w-3/4 justify-around my-5 lg:flex-row'>
-      <div className='flex flex-col'>
-        <h2 className='text-2xl'>S4S</h2>
-        <p className='text-sm'>© 2021 Students 4 Students</p>
-        <p className='text-sm'>+1 949 864 9615</p>
-        <p className='text-sm'>studs4students@gmail.com</p>
-      </div>
-      <div className='flex flex-col'>
-        <h2 className='text-xl font-bold'>Programs</h2>
-        <p className='text-sm'>by Littles</p>
-        <p className='text-sm'>Project Jam</p>
-        <h2 className="text-xl font-bold">Gallery</h2>
-        <h2 className="text-xl font-bold">About</h2>
-      </div>
-      <div className='flex flex-col'>
-        <div className="flex-grow"></div>
+    <div className='footer my-10 flex flex-row justify-center'>
+      <div className='my-5 flex w-3/4 flex-col justify-around lg:flex-row'>
+        <div className='flex flex-col'>
+          <h2 className='text-2xl'>S4S</h2>
+          <p className='text-sm'>© 2021 Students 4 Students</p>
+          <p className='text-sm'>+1 949 864 9615</p>
+          <p className='text-sm'>studs4students@gmail.com</p>
+        </div>
+        <div className='flex flex-col'>
+          <Link href={t('programs_link')}>
+            <h2 className='text-xl font-bold'>Programs</h2>
+          </Link>
+
+          <Link href={`${t('programs_link')}#BL`}>
+            <p className='text-sm'>Big Little Program</p>
+          </Link>
+
+          <Link href={`${t('programs_link')}#PJ`}>
+            <p className='text-sm'>Project Jam</p>
+          </Link>
+          <Link href={t('gallery_link')}>
+            <h2 className='text-xl font-bold'>Gallery</h2>
+          </Link>
+          <Link href={t('about_link')}>
+            <h2 className='text-xl font-bold'>About</h2>
+          </Link>
+        </div>
+        <div className='flex flex-col'>
+          <div className='flex-grow'></div>
           <p>&quot;College started yesterday, but you can start today&quot;</p>
+        </div>
       </div>
     </div>
-</div>
   )
 }
