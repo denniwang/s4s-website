f689c5e7ef6d9ef305def8fa221a2d09b593b749
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-02-04 12:23:29 -0500

    testing longer list to center items

--- src/app/[locale]/components/CollegeLogos.tsx
+++ src/app/[locale]/components/CollegeLogos.tsx
@@ -8,6 +8,10 @@ const collegeLogos = [
   { image: '/logos/cal.png', alt: 'Cal' },
   { image: '/logos/ucla.svg', alt: 'UCLA' },
   { image: '/logos/uci.svg', alt: 'UCI' },
+  { image: '/logos/yale.svg', alt: 'Yale' },
+  { image: '/logos/cal.png', alt: 'Cal' },
+  { image: '/logos/ucla.svg', alt: 'UCLA' },
+  { image: '/logos/uci.svg', alt: 'UCI' },
   { image: '/logos/yale.svg', alt: 'Yale' }
 ]
 
@@ -15,7 +19,7 @@ export const CollegeLogos = () => {
   return (
     <div className='w-full bg-white py-4 md:py-7'>
       <motion.div
-        className='flex w-full justify-center overflow-hidden'
+        className='align-center flex w-full justify-center overflow-hidden'
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2 }}
